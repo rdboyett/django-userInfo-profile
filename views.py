18db64d79eaaf36fe2cd7c07fb5c6b5428a943ab
rdboyett <rdboyett@gmail.com>
2014-05-15 13:19:08 -0500

    added startup info

--- views.py
+++ views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import os
+ROOT_PATH = os.path.dirname(__file__)
 
-# Create your views here.
+from django.shortcuts import render_to_response, redirect, get_object_or_404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+from userInfo_profile.models import *
+from userInfo_profile import settings
+
+
+
+def test(request):
+    return HttpResponse('You are in test in userInfo_profile views.py')
+
+def test_function():
+    return 'you got this from test_function inside userInfo_profile views.py'
